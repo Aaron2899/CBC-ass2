a00e32ac2b9a9ce1aab38dbe77c7207dbc1aa3ff
aaron2899 <aaronmishima@gmail.com>
2024-08-06 15:35:30 -0700

    first commit

---
+++ src/firstcode/main.java
@@ -0,0 +1,35 @@
+package firstcode;
+
+public class main {
+    public static void main(String[] args) {
+        int[] foo = {0, 1, 2, 3, 4};
+
+        int[] oneHundred = new int[100];
+        for(int i = 0; i < 100; i++){
+            oneHundred[i] = i;
+        }
+        System.out.println("The number of even numbers in foo is:");
+        System.out.println(countEvenNum(foo, 5));
+
+        System.out.println("The number of even numbers in oneHundred is:");
+        System.out.println(countEvenNum(oneHundred, 100));
+
+    }
+
+
+
+    private static int countEvenNum(int[] arr, int size){
+        int count = 0;
+
+        for(int i = 0; i < size; i++){
+            if(arr[i] % 2 == 0){
+                count++;
+            }
+            else{
+
+            }
+        }
+
+        return count;
+    }
+}
